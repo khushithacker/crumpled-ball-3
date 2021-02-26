fb26461479d8941db7487a2e5628f541ede1c7cf
khushithacker <71810632+khushithacker@users.noreply.github.com>
2021-02-26 18:04:53 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,56 @@
+var dustbinObj;
+var ground;
+var ball;
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	
+}
+
+function setup() {
+  rectMode(CENTER);
+	createCanvas(1200, 600);
+
+  engine = Engine.create();
+	world = engine.world;
+
+  ground = new Ground(600,570,1200,20);
+  dustbinObj = new Dustbin(1000,550);
+  ball=new  PaperBall(80,450);
+  
+	Engine.run(engine);
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background("lightgrey");
+  Engine.update(engine);
+ 
+  ground.display();
+  dustbinObj.display();
+  ball.display();
+  launcher.display();
+
+  drawSprites();
+
+}
+
+function keyPressed() {
+if (keyCode=== UP_ARROW) {
+  Matter.Body.applyForce(ball.body,ball.body.position,{x:190,y:-195});
+}
+
+function mouseDragged(){
+  Matter.Body.setPosition(PaperBall.body,{x:mouseX,y:mouseY});
+}
+
+function mouseReleased(){
+  launcher.fly();
+}
+}
+
